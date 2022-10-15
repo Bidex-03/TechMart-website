038b482a02d04495cdb71abaac943b147d69de71
Bidex-03 <abdullahqaasim14@gmail.com>
2022-10-15 14:01:39 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Toggle menu
+
+var sideLink = document.getElementById('side-link');
+
+function showMenu() {
+    sideLink.style.right = "0";
+}
+
+function hideMenu() {
+    sideLink.style.right = "-200px";
+}
