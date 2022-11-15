5e35a85eb1b284e34bad76250cfb3ce0d22a4752
Bidex-03 <abdullahqaasim14@gmail.com>
2022-11-15 01:29:03 +0100

    changes

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function hideMenu() {
     sideLink.style.right = "-200px";
 }
 
+///////////////////////
+// Quick auctions
 const containerAuctions = document.querySelector("#auctions");
 const containerAuctionImages = document.querySelector(".auction-images");
 
@@ -47,13 +49,88 @@ const displayAuction = (auctions) => {
         const auctionImage = document.createElement("div");
         auctionImage.classList.add("auction-image");
         auctionImage.innerHTML = `
-            <img src="${auction.image}" alt="${auction.image}">
+            <a href="#"><img src="${auction.image}" alt="${auction.name}">
             <div class="auction-info">
                 <h3>${auction.name}</h3>
                 <p>${auction.price}</p>
-            </div>
+            </div></a>
         `;
         containerAuctionImages.appendChild(auctionImage);
     });
 }
 displayAuction(auctions);
+
+
+//////////////////////////
+// Categories
+const containerCategories = document.querySelector("#categories");
+const containerCategoryImages = document.querySelector(".category-images");
+
+let categories = [
+    {
+        name: "Fashion Accessories",
+        image: "./Asset/categories-fashion.jpg",
+    },
+    {
+        name: "Computer Systems",
+        image: "./Asset/categories-computer.jpg",
+    },
+    {
+        name: "Electronics",
+        image: "./Asset/categories-electronics.jpg",
+    },
+    {
+        name: "Photography",
+        image: "./Asset/categories-photography.png",
+    },
+    {
+        name: "Furnitures",
+        image: "./Asset/categories-furniture.jpg",
+    },
+    {
+        name: "Vehicles and Accessories",
+        image: "./Asset/categories-vehicles.jpg",
+    },
+    {
+        name: "Toys and Hobbies",
+        image: "./Asset/categories-toys.jpg",
+    },
+    {
+        name: "Games and Accessories",
+        image: "./Asset/categories-games.jpg",
+    },
+    {
+        name: "Tools and Accessories",
+        image: "./Asset/categories-tools.jpg",
+    },
+    {
+        name: "Lighting and Accessories",
+        image: "./Asset/categories-lighting.jpg",
+    },
+    {
+        name: "Tech Gadgets",
+        image: "./Asset/categories-tech.jpg",
+    },
+    {
+        name: "Mobile Phones",
+        image: "./Asset/categories-mobile.jpg",
+    }
+]
+
+const displayCategories = (categories) => {
+    containerCategoryImages.innerHTML = "";
+
+    categories.forEach(function (category) {
+        const categoryImage = document.createElement("div");
+        categoryImage.classList.add("category-image");
+        categoryImage.innerHTML = `
+            <a href="#"><img src="${category.image}" alt="${category.name}">
+            <div class="category-info">
+                <p>${category.name}</p>
+            </div></a>
+        `;
+        containerCategoryImages.appendChild(categoryImage);
+    });
+    
+}
+displayCategories(categories);
